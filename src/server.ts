326d7067807a0e6dea6d132ba52ecd99531ea7c7
Pietro Gonçalves <pietro_goncalves@hotmail.com>
2021-09-16 09:13:52 -0300

    categoriesroute

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import {categoriesRoutes} from './routes/categories.routes';
+
 
 const app = express();
 
+app.use(express.json());
+
+app.use(categoriesRoutes);
+
+
 app.listen(3333, () => {
   console.log("Server OK (3333) 🚀");
-})
+});
